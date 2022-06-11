9bf7940f24b89721080db4317b9adb11d98bd845
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2022-06-11 17:01:31 +0900

    フリー画像検索アプリ【後編】 (#2)
    * TextFormFieldを追加
    
    * 検索できるようにする
    
    * Stackを使ってレイアウト調整
    
    * 画像をタップできるようにする
    
    * ダウンロード機能・シェア機能を追加

--- lib/ui/page/pixabay_page.dart
+++ lib/ui/page/pixabay_page.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:free_image_search_app/utils/constants.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 class PixabayPage extends StatefulWidget {
   const PixabayPage({Key? key}) : super(key: key);
@@ -16,12 +20,25 @@ class _PixabayPageState extends State<PixabayPage> {
   @override
   void initState() {
     super.initState();
-    fetchImage();
+    fetchImage('');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          decoration: const InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+            hintText: '入力してみよう',
+          ),
+          onFieldSubmitted: (text) {
+            print(text);
+            fetchImage(text);
+          },
+        ),
+      ),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
@@ -29,14 +46,57 @@ class _PixabayPageState extends State<PixabayPage> {
         itemCount: imageList.length,
         itemBuilder: (context, index) {
           Map<String, dynamic> image = imageList[index];
-          return Image.network(image['previewURL']);
+          return InkWell(
+            onTap: () async {
+              // 一時保存に使えるフォルダ情報を取得する
+              Directory dir = await getTemporaryDirectory();
+
+              Response response = await Dio().get(
+                image[('webformatURL')],
+                options: Options(
+                  // 画像をダウンロードするときは ResponseType.bytes を指定します。
+                  responseType: ResponseType.bytes,
+                ),
+              );
+              // フォルダの中に image.png という名前でファイルを作り、そこに画像データを書き込む。
+              File imageFile = await File('${dir.path}/image.png').writeAsBytes(response.data);
+
+              // path指定してシェアする
+              await Share.shareFiles([imageFile.path]);
+            },
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Image.network(
+                  image['previewURL'],
+                  fit: BoxFit.cover,
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                    color: Colors.white,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(
+                          Icons.thumb_up_alt_outlined,
+                          size: 14,
+                        ),
+                        Text('${image['likes']}'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
         },
       ),
     );
   }
 
-  Future<void> fetchImage() async {
-    final response = await Dio().get(Constants.baseAPI);
+  Future<void> fetchImage(String text) async {
+    final response = await Dio().get(Constants().baseAPI(text));
 
     imageList = response.data['hits'];
     // 画像を更新
