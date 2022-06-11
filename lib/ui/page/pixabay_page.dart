39028fced0a9b0739e0c631ac509c8692381d6d2
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-06-11 15:02:52 +0900

    PixabayPageを作成

---
+++ lib/ui/page/pixabay_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PixabayPage extends StatefulWidget {
+  const PixabayPage({Key? key}) : super(key: key);
+
+  @override
+  State<PixabayPage> createState() => _PixabayPageState();
+}
+
+class _PixabayPageState extends State<PixabayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
