50f1fb43c3f164e489535468381002b1c51073be
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-06-11 15:08:49 +0900

    JSONデータをコンソールに表示する

--- lib/ui/page/pixabay_page.dart
+++ lib/ui/page/pixabay_page.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:free_image_search_app/utils/constants.dart';
 
 class PixabayPage extends StatefulWidget {
   const PixabayPage({Key? key}) : super(key: key);
@@ -9,7 +11,18 @@ class PixabayPage extends StatefulWidget {
 
 class _PixabayPageState extends State<PixabayPage> {
   @override
+  void initState() {
+    super.initState();
+    fetchImage();
+  }
+  @override
   Widget build(BuildContext context) {
     return const Scaffold();
+  }
+  
+  Future<void> fetchImage() async{
+    final response = await Dio().get(Constants.baseAPI);
+
+    print(response.data);
   }
 }
