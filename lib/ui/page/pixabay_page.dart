953e0ef991fa4d7cae8f51b9b51b599f76dab945
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2022-06-11 15:34:49 +0900

    フリー画像検索アプリ【前編】 (#1)
    * dioを追加
    
    * PixabayPageを作成
    
    * JSONデータをコンソールに表示する
    
    * 画像を表示させる

---
+++ lib/ui/page/pixabay_page.dart
@@ -0,0 +1,45 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:free_image_search_app/utils/constants.dart';
+
+class PixabayPage extends StatefulWidget {
+  const PixabayPage({Key? key}) : super(key: key);
+
+  @override
+  State<PixabayPage> createState() => _PixabayPageState();
+}
+
+class _PixabayPageState extends State<PixabayPage> {
+  // 取得した画像データを入れる
+  List imageList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchImage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemCount: imageList.length,
+        itemBuilder: (context, index) {
+          Map<String, dynamic> image = imageList[index];
+          return Image.network(image['previewURL']);
+        },
+      ),
+    );
+  }
+
+  Future<void> fetchImage() async {
+    final response = await Dio().get(Constants.baseAPI);
+
+    imageList = response.data['hits'];
+    // 画像を更新
+    setState(() {});
+  }
+}
